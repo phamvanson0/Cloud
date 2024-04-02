600f8fb0d0dd46becb5ed28d1d96dc267110ef51
PhamVanSon02 <107165698+phamvanson0@users.noreply.github.com>
2024-04-02 07:14:34 +0700

    update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
 <?php
-    $conn = mysqli_connect("p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "pq7u3y4vbacg64ti", "	p2g3bztlltef9up3", "viofo4vqp0gclwk0")
-            or die("Cannot connect database".mysqli_connect_error());     
+    $conn = mysqli_connect("jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "k5s4tlcae6rkxopr", "ristiteyev9lcwqs", "xk8oibhzbbmbykmk") 
+    or die("Cannot connect database".mysqli_connect_error());     
+    //mysql://k5s4tlcae6rkxopr:ristiteyev9lcwqs@jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xk8oibhzbbmbykmk
+    mysqli_query($conn,'SET NAMES "utf8"');
+    //mysql_close($conn);
 ?>
